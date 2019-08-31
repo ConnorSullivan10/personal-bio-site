e90942bde5de8ef3e51608de69b67d5eb1719856
Connor Sullivan <cksullvn@gmail.com>
2019-08-31 13:26:10 -0500

    corrected if Available function in JS so that only available Project cards are printed

--- main.js
+++ main.js
@@ -8,20 +8,28 @@ const projects = [
     technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
     available: true,
     url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
-  }
+    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux",
+  },
+  {
+    title: "UnCool Project", 
+    screenshot: "http://english.cctv.com/20090709/images/1247100408117_1247100408117_r.jpg", 
+    description: "Oh noooooo, don't look. This is the unavailable project!", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: false,
+    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux",
+  },
 ]
 
 const printToDom = (toPrint, divId) => {
   document.getElementById(divId).innerHTML = toPrint;
 };
 
-
-
 const createProjectCards = (projectsArr) => {
-  let domString = ''
+  let domString = '';
   for (let i = 0; i < projectsArr.length; i++) {
-      const project = projectsArr[i]
+      let project = projectsArr[i]
+      if (project.available) {
       domString += `
           <div class="projects-card">
               <h2>${project.title}</h2>
@@ -31,17 +39,8 @@ const createProjectCards = (projectsArr) => {
               <a href=${project.url}>Project</a>
               <a href=${project.githubUrl}>GitHub</a>
           </div>
-      `
-      const availableProjectCards = []
-              for (let i = 0; i < projects.length; i ++) {
-                  const project = projects[i]
-                  if (project.available === true) {
-                      availableProjectCards.push(project)
-                  }
-  
-    }
+      `} 
+      }
   printToDom(domString, 'projectsPage')
-}
-};
-
+    };
 createProjectCards(projects);
