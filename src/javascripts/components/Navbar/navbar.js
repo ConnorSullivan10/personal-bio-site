c319a45e11f4c2aa07eb295aea5c263fad3aa57c
Connor Sullivan <cksullvn@gmail.com>
2020-01-02 12:51:12 -0600

    fixed styling of background image on home page

--- src/javascripts/components/Navbar/navbar.js
+++ src/javascripts/components/Navbar/navbar.js
@@ -1,9 +1,19 @@
 import $ from 'jquery';
 import './navbar.scss';
 
+const displayHome = () => {
+  $('#navToHome').on('click', () => {
+    $('#homePage').show();
+    $('#bioPage').hide();
+    $('#technologiesPage').hide();
+    $('#projectsPage').hide();
+  });
+};
+
 const displayBio = () => {
   $('#navToBio').on('click', () => {
     $('#bioPage').show();
+    $('#homePage').hide();
     $('#technologiesPage').hide();
     $('#projectsPage').hide();
   });
@@ -12,6 +22,7 @@ const displayBio = () => {
 const displayTechnologies = () => {
   $('#navToTechnologies').on('click', () => {
     $('#technologiesPage').show();
+    $('#homePage').hide();
     $('#bioPage').hide();
     $('#projectsPage').hide();
   });
@@ -20,12 +31,14 @@ const displayTechnologies = () => {
 const displayProjects = () => {
   $('#navToProjects').on('click', () => {
     $('#projectsPage').show();
+    $('#homePage').hide();
     $('#bioPage').hide();
     $('#technologiesPage').hide();
   });
 };
 
 const attachNavbarEvents = () => {
+  displayHome();
   displayBio();
   displayTechnologies();
   displayProjects();
